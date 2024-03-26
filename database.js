0ed8b1a133e43305a7538fdf3e778d90b9b1f0bd
Roger <mhsieh32@my.bcit.ca>
2024-03-26 15:45:25 -0700

    Updated database.js

--- database.js
+++ database.js
@@ -1,14 +1,47 @@
-let Database = {
-  cindy: {
-    reminders: [
-      {
-        id: 1,
-        title: "Grocery shopping",
-        description: "Buy milk and bread from safeway",
-        completed: false,
-      },
-    ],
-  },
-};
-
 module.exports = Database;
+
+const database = [
+  {
+      id: 1,
+      name: "Cindy Park",
+      email: "cindy123@gmail.com",
+      password: "cindy123!",
+      reminders: [],
+      role: "admin",
+  },
+  {
+      id: 2,
+      name: "Johnny Doe",
+      email: "johnny123@gmail.com",
+      password: "johnny123!",
+      reminders: [],
+      role:"user",
+  },
+  {
+      id: 3,
+      name: "Jonathan Chen",
+      email: "jonathan123@gmail.com",
+      password: "jonathan123!",
+      reminders: [],
+      role:"user",
+  },
+  ];
+  
+  const userModel = {
+  findOne: (email) => {
+      const user = database.find((user) => user.email === email);
+      if (user) {
+      return user;
+      }
+      throw new Error(`Couldn't find user with email: ${email}`);
+  },
+  findById: (id) => {
+      const user = database.find((user) => user.id === id);
+      if (user) {
+      return user;
+      }
+      throw new Error(`Couldn't find user with id: ${id}`);
+  },
+  };
+  
+  module.exports = { database, userModel };
